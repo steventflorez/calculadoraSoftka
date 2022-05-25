9abd07e8a54dfc250d8c5dc22fcc4588abbfceec
juandavidamayaardila <juandavidamayaardila@gmail.com>
2022-05-25 15:46:55 -0500

    se agrego documentacion javadoc

---
+++ calculadora/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+N
+src/main/java/com/co/Main.java,d\e\de7ce84ffac4100da28b82d86a17c5a55436dd4c
--- calculadora/src/main/java/com/co/Main.java
+++ calculadora/src/main/java/com/co/Main.java
@@ -5,16 +5,34 @@ import java.util.logging.Logger;
 
 public class Main {
 
+    /*
+     Constante que permite mostrar informacion en consola
+     */
     private static final Logger log = Logger.getLogger("log");
+    /*
+    Constante que permite caputrar texto desde la consola.
+     */
     private static final Scanner capture = new Scanner(System.in);
 
+    /**
+     * Metodo que permite inicilizar la aplicacion.
+     *
+     * @param args atributo para iniciar la aplicacion
+     */
     public static void main(String[] args) {
 
         /*
-        se crean variables para la suma de dos numeros
-        * */
+         * Atributo para almacenar el primer numero.
+         */
         double number1;
+
+        /*
+        Atributo para almacenar el segundo numero ingresado.
+         */
         double number2;
+        /*
+        Atributo para indentificar la operacion deseada
+         */
         int operator;
 
         log.info("********CALCULADORA SOFKA*******");
@@ -34,11 +52,18 @@ public class Main {
                 """);
 
         operator = capture.nextInt();
-        calculate(operator,number1,number2);
+        calculate(operator, number1, number2);
 
     }
 
-    public static void calculate(int operator, double number1, double number2){
+    /**
+     * invoca la operacion seleccionada por el usuario
+     *
+     * @param operator indica que operacion se va a realizar
+     * @param number1  almacena el primer numero ingresado
+     * @param number2  almacena el segundo numero ingresado
+     */
+    public static void calculate(int operator, double number1, double number2) {
         switch (operator) {
             case 1 -> suma(number1, number2);
             case 2 -> resta(number1, number2);
@@ -48,33 +73,66 @@ public class Main {
             default -> print(0);
         }
     }
-    //operaciones matematicas
-    public static void suma(double number1, double number2){
-       print(number1 + number2);
+
+    /**
+     * este metodo nos permite genear la suma de dos numero
+     *
+     * @param number1 primer numero ingresado por el usuario
+     * @param number2 segundo numero ingresado por el usuario
+     */
+    public static void suma(double number1, double number2) {
+        print(number1 + number2);
     }
 
-    public static void resta(double number1, double number2){
+    /**
+     * Este metodo nos permite generar la resta de los dos numeros
+     *
+     * @param number1 primer numero ingresado por el usuario
+     * @param number2 segundo numero ingresado por el usuario
+     */
+    public static void resta(double number1, double number2) {
         print(number1 - number2);
     }
 
-    public static void multi(double number1, double number2){
+    /**
+     * Este metodo nos permite generar la multiplicacion de los dos numeros
+     *
+     * @param number1 primer numero ingresado por el usuario
+     * @param number2 segundo numero ingresado por el usuario
+     */
+    public static void multi(double number1, double number2) {
         print(number1 * number2);
     }
 
-    public static void divi(double number1, double number2){
+    /**
+     * Este metodo nos permite generar la divicion de los dos numeros
+     *
+     * @param number1 primer numero ingresado por el usuario
+     * @param number2 segundo numero ingresado por el usuario
+     */
+    public static void divi(double number1, double number2) {
         print(number1 / number2);
     }
 
-    public static void all(double number1, double number2){
+    /**
+     * Este metodo nos permite generar todas las operaciones de los dos numeros
+     *
+     * @param number1 primer numero ingresado por el usuario
+     * @param number2 segundo numero ingresado por el usuario
+     */
+    public static void all(double number1, double number2) {
         print(number1 + number2);
         print(number1 - number2);
         print(number1 * number2);
         print(number1 / number2);
     }
 
-    public static void print(double result){
+    /**
+     * Metodo que permite imprimir el resultado
+     *
+     * @param result resultado a imprmir
+     */
+    public static void print(double result) {
         log.info("El result es: " + result);
     }
-
-
 }
